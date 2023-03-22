3b11ce21306a37c612e6069a4a2ea67beeac9b5c
tongwanming <tongwanming@wewin.com.cn>
2023-03-22 17:20:05 +0800

    first commit

---
+++ AlphaEventDemo/AlphaEventImageView.h
@@ -0,0 +1,16 @@
+//
+//  AlphaEventImageView.h
+//  AlphaEventDemo
+//
+//  Created by MakeID-研发 on 2023/3/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AlphaEventImageView : UIImageView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AlphaEventDemo/AlphaEventImageView.m
@@ -0,0 +1,20 @@
+//
+//  AlphaEventImageView.m
+//  AlphaEventDemo
+//
+//  Created by MakeID-研发 on 2023/3/22.
+//
+
+#import "AlphaEventImageView.h"
+
+@implementation AlphaEventImageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
