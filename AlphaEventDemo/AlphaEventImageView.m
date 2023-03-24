3ad5d6ab2d5fd0b31a5ecb09d0047ba419e3b90e
wanming tong <359209162@qq.com>
2023-03-24 09:13:06 +0800

    fix
    first commit

--- AlphaEventDemo/AlphaEventImageView.m
+++ AlphaEventDemo/AlphaEventImageView.m
@@ -9,12 +9,40 @@
 
 @implementation AlphaEventImageView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)initWithImage:(UIImage *)image
+{
+    if (self = [super initWithImage:image]) {
+        self.userInteractionEnabled = YES;
+    }
+    return self;
 }
-*/
+
+- (BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event {
+    //判断当前手势是否点击在当前imageView 上面
+//    NSLog(@"响应的view的他给：%ld",(long)self.tag);
+    BOOL con = CGRectContainsPoint(self.bounds, point);
+    if (con) {
+        
+        unsigned char pixel[1] = {0};
+        CGContextRef context = CGBitmapContextCreate(pixel,
+        1, 1, 8, 1, NULL,
+        kCGImageAlphaOnly);
+        UIGraphicsPushContext(context);
+        //这一部很重要，因为去drawAtPoint 是根据图片本身大小去的，所以需要算一个比例值
+        CGFloat scW = self.image.size.width/self.bounds.size.width;
+        CGFloat scH = self.image.size.height/self.bounds.size.height;
+        [self.image drawAtPoint:CGPointMake(-point.x * scW, -point.y * scH)];
+        UIGraphicsPopContext();
+        CGContextRelease(context);
+        CGFloat alpha = pixel[0]/255.0f;
+        //alpha 小于0.01f，认为是透明的
+        BOOL transparent = alpha < 0.01f;
+//        NSLog(@"当前的透明度：%lf",alpha);
+        return !transparent;
+    } else {
+//        NSLog(@"没有点击在图片上面，对应的tag：%ld",(long)self.tag);
+        return NO;
+    }
+}
 
 @end
--- AlphaEventDemo/ViewController.m
+++ AlphaEventDemo/ViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "ViewController.h"
+#import "AlphaEventImageView.h"
 
 @interface ViewController ()
 
@@ -15,7 +16,54 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    AlphaEventImageView *alphaImage1 = [[AlphaEventImageView alloc] initWithImage:[UIImage imageNamed:@"test1"]];
+    alphaImage1.tag = 1001;
+    alphaImage1.frame = CGRectMake(100, 100, 100, 100);
+    UITapGestureRecognizer *tap1 = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap1Click)];
+    [alphaImage1 addGestureRecognizer:tap1];
+    [self.view addSubview:alphaImage1];
+    
+    
+    AlphaEventImageView *alphaImage2 = [[AlphaEventImageView alloc] initWithImage:[UIImage imageNamed:@"test2"]];
+    alphaImage2.tag = 1002;
+    alphaImage2.frame = CGRectMake(100, 300, 100, 100);
+    [self.view addSubview:alphaImage2];
+    UITapGestureRecognizer *tap2 = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap2Click)];
+    [alphaImage2 addGestureRecognizer:tap2];
+    
+    
+    AlphaEventImageView *alphaImage3 = [[AlphaEventImageView alloc] initWithImage:[UIImage imageNamed:@"test1"]];
+    alphaImage3.tag = 1003;
+    alphaImage3.frame = CGRectMake(100, 500, 100, 100);
+    UITapGestureRecognizer *tap3 = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap3Click)];
+    [alphaImage3 addGestureRecognizer:tap3];
+    [self.view addSubview:alphaImage3];
+    
+    
+    AlphaEventImageView *alphaImage4 = [[AlphaEventImageView alloc] initWithImage:[UIImage imageNamed:@"test2"]];
+    alphaImage4.tag = 1004;
+    alphaImage4.frame = CGRectMake(100, 550, 100, 100);
+    [self.view addSubview:alphaImage4];
+    UITapGestureRecognizer *tap4 = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap4Click)];
+    [alphaImage4 addGestureRecognizer:tap4];
     // Do any additional setup after loading the view.
+}
+
+- (void)tap1Click {
+    NSLog(@"点击兔子的不透明部分");
+}
+
+- (void)tap2Click {
+    NSLog(@"点击baby的不透明部分");
+}
+
+- (void)tap3Click {
+    NSLog(@"点击兔子的不透明部分");
+}
+
+- (void)tap4Click {
+    NSLog(@"点击baby的不透明部分");
 }
 
 
